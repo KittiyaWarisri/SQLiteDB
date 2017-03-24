d2c1464ed76351d224c619594366dcfc24cdbb39
Preaw-PC <kittiya000preaw@gmail.com>
2017-03-24 18:16:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/preaw_pc/sqlitedb/Graph.java
@@ -0,0 +1,63 @@
+package com.example.preaw_pc.sqlitedb;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Graph extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        StudentRepo repo = new StudentRepo(this);
+
+        ArrayList<HashMap<String, String>> studentList =  repo.getStudentList();
+        if(studentList.size()!=0) {
+            DataPoint a[]=new DataPoint[studentList.size()];
+           
+        }else{
+            Toast.makeText(this,"No student!",Toast.LENGTH_SHORT).show();
+        }
+
+        graph.addSeries(series);
+    }
+
+}
---
+++ app/src/main/java/com/example/preaw_pc/sqlitedb/Student.java
@@ -0,0 +1,26 @@
+package com.example.preaw_pc.sqlitedb;
+
+/**
+ * Created by Preaw-PC on 23/3/2560.
+ */
+
+public class Student {
+
+    // Labels table name
+    public static final String TABLE = "Student";
+
+    // Labels Table Columns names
+    public static final String KEY_ID = "id";
+    public static final String KEY_name = "name";
+    public static final String KEY_email = "email";
+    public static final String KEY_age = "age";
+
+    // property help us to keep data
+    public int student_ID;
+    public String name;
+    public String email;
+    public int age;
+
+
+
+}
